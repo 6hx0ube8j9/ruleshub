0bf021b344c2f2222a0ae9423fe1e6839b571fa3
6hx0ube8j9 <84956360+6hx0ube8j9@users.noreply.github.com>
2026-07-06 23:37:17 +0800

    Update build_rules.py

--- build_rules.py
+++ build_rules.py
@@ -746,11 +746,10 @@ def main():
     # PAC
     for g_name, raw_domains in global_matrix['pac'].items():
         pac_path = os.path.join(PAC_DIR, f"{g_name}.pac")
-		
-        if isinstance(g_domains, dict):
-            combined_domains = set(g_domains.get('full', [])) | set(g_domains.get('suffix', []))
+        if isinstance(raw_domains, dict):
+            combined_domains = set(raw_domains.get('full', [])) | set(raw_domains.get('suffix', []))
         else:
-            combined_domains = set(g_domains) if g_domains else set()
+            combined_domains = set(raw_domains) if raw_domains else set()
 
         direct_domains = sorted(list(combined_domains))
         
